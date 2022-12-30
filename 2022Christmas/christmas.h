93c708c5bdfaaa0a991b507d65e70129e03c2c11
Dylan Suen <suen333robot@gmail.com>
2022-12-30 23:13:54 +0800

    2022 正義高中 聖誕題目

---
+++ 2022Christmas/christmas.h
@@ -0,0 +1,189 @@
+
+#ifndef CLION_CHRISTMAS_H
+#define CLION_CHRISTMAS_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+using pii = pair<int, int>;
+using map_type = vector<vector<int>>;
+
+class christmas {
+    struct shape {
+        unsigned long long size;
+        bool reversible;
+        bool turnable;
+        vector<pii> points;
+
+        shape(const vector<pii> &points, bool reversible, bool turnable) : reversible(reversible),
+                                                                           turnable(turnable),
+                                                                           points(points),
+                                                                           size(points.size()) {}
+
+        explicit shape(const vector<pii> &point) : shape(point, true, true) {}
+    };
+
+    inline int possibleRun(map_type &_map_, int x, int y) {
+        if (x < 0 || x > 6 || y < 0 || y > 6 || _map_[x][y])
+            return 0;
+        _map_[x][y] = 1;
+        return possibleRun(_map_, x - 1, y) +
+               possibleRun(_map_, x + 1, y) +
+               possibleRun(_map_, x, y + 1) +
+               possibleRun(_map_, x, y - 1) + 1;
+    }
+
+    inline bool possible(int min_size) {
+        auto _map_ = map;
+        int ret = width * height;
+        for (int x = 0; x < 7; ++x)
+            for (int y = 0; y < 7; ++y) {
+                if (_map_[x][y])
+                    continue;
+                if (possibleRun(_map_, x, y) < min_size)
+                    return false;
+            }
+        return true;
+    }
+
+    static inline pii cvt(const pii &start, const int dir, const pii &vec, bool reverse = false) {
+        if (reverse)
+            switch (dir) {
+                case 0:
+                    return {start.first - vec.first, start.second + vec.second};
+                case 1:
+                    return {start.first - vec.second, start.second - vec.first};
+                case 2:
+                    return {start.first + vec.first, start.second - vec.second};
+                case 3:
+                    return {start.first + vec.second, start.second + vec.first};
+                default:
+                    return {-1, -1};
+            }
+        else
+            switch (dir) {
+                case 0:
+                    return {start.first + vec.first, start.second + vec.second};
+                case 1:
+                    return {start.first + vec.second, start.second - vec.first};
+                case 2:
+                    return {start.first - vec.first, start.second - vec.second};
+                case 3:
+                    return {start.first - vec.second, start.second + vec.first};
+                default:
+                    return {-1, -1};
+            }
+    }
+
+    vector<pii> preprocessing(vector<pii> &now, int x, int y, int dir, bool rev) {
+        vector<pii> reg;
+        for (const auto &regi: now) {
+            auto ret = cvt({x, y}, dir, regi, rev);
+            if (ret.first < 0 || ret.first > 6 || ret.second < 0 || ret.second > 6 ||
+                map[ret.first][ret.second]) {
+                return {};
+            }
+            reg.emplace_back(ret);
+        }
+        return reg;
+    }
+
+
+    vector<map_type> ans;
+
+    unsigned long long width{}, height{};
+
+public:
+    const vector<map_type> &getAns() const {
+        return ans;
+    }
+
+    vector<shape> blocks;
+    map_type map;
+
+    inline void add_block(vector<string> init_strings, bool line_symmetry, bool point_symmetry) {
+        auto x0 = init_strings.front().find_first_of('+');
+        vector<pii> new_shape;
+        for (int i = 0; i < init_strings.size(); ++i) {
+            auto _i = init_strings[i];
+            for (int j = 0; j < _i.size(); ++j) {
+                if (_i[j] == '+')
+                    new_shape.emplace_back(i, j - x0);
+            }
+        }
+        blocks.emplace_back(new_shape, !line_symmetry, !point_symmetry);
+    }
+
+    inline void setMap(const vector<string> &map) {
+        christmas::map.clear();
+        for (auto &i: map) {
+            vector<int> map_line;
+            for (auto &j: i) {
+                if (j == 'X')
+                    map_line.push_back(-1);
+                else
+                    map_line.push_back(0);
+            }
+            christmas::map.emplace_back(map_line);
+        }
+        height = map.size();
+        width = map.front().size();
+    }
+
+//init operations before run
+    inline void init() {
+        std::sort(blocks.begin(), blocks.end(),
+                  [](const shape &a, const shape &b) { return a.size < b.size; });
+    }
+
+
+    void run(int idx = 0) {
+        if (idx == blocks.size()) {
+            ans.push_back(map);
+            return;
+        }
+        auto &now = blocks[idx];
+        if (!possible(now.size))
+            return;
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                if (map[i][j])
+                    continue;
+                bool rev = false;
+                again:
+                for (int dir = 0; dir < (now.turnable ? 4 : 2); ++dir) {
+                    auto reg = preprocessing(now.points, i, j, dir, rev);
+                    if (reg.empty())
+                        continue;
+                    for (auto &regi: reg)
+                        map[regi.first][regi.second] = idx + 1;
+                    run(idx + 1);
+                    for (auto &regi: reg)
+                        map[regi.first][regi.second] = 0;
+                }
+                if (rev != now.reversible) {
+                    rev = true;
+                    goto again;
+                }
+            }
+        }
+    }
+};
+
+basic_ostream<char> &operator<<(basic_ostream<char> &out, const map_type &map) {
+    for (auto &i: map) {
+        for (auto &j: i)
+            if (j == -1)
+                out << '0' << ' ';
+            else
+                out << j << ' ';
+        out << '\n';
+    }
+    return out;
+}
+
+#endif //CLION_CHRISTMAS_H
--- 2022Christmas/main.cpp
+++ 2022Christmas/main.cpp
@@ -1,204 +1,29 @@
 #include <bits/stdc++.h>
-
+#include "christmas.h"
 using namespace std;
-
-class christmas {
-
-    using pii = pair<int, int>;
-    basic_ostream<char> &out;
-
-    struct shape {
-        vector<pii> points;
-
-        shape(const initializer_list<pii> &point) : points(point) {}
-    };
-
-    vector<shape> blocks{shape{{0, 0}, //+++
-                               {0, 1}, //+
-                               {0, 2}, //+
-                               {1, 0}, //
-                               {2, 0}},
-                         shape{{0, 0}, //++
-                               {0, 1}, // +
-                               {1, 1}, // +
-                               {2, 1}, // +
-                               {3, 1}},
-                         shape{{0, 0}, // +
-                               {1, -1},//++
-                               {1, 0}, // +
-                               {2, 0}, // +
-                               {3, 0}},
-                         shape{{0, 0}, // +
-                               {1, -1},//++
-                               {1, 0}, //+
-                               {2, -1},//+
-                               {3, -1}},
-                         shape{{0, 0}, //++
-                               {0, 1}, //+++
-                               {1, 0}, //
-                               {1, 1}, //
-                               {1, 2}},
-                         shape{{0, 0}, //+ +
-                               {0, 2}, //+++
-                               {1, 0}, //
-                               {1, 1}, //
-                               {1, 2}},
-                         shape{{0, 0}, // ++
-                               {0, 1}, // +
-                               {1, 0}, //++
-                               {2, -1},//
-                               {2, 0}},
-                         shape{{0, 0}, //++
-                               {0, 1}, //++
-                               {1, 0}, //++
-                               {1, 1}, //
-                               {2, 0}, //
-                               {2, 1}}};
-
-    vector<bool> reversible{false, true, true, true, true, false, true, false};
-    vector<bool> turnable{true, true, true, true, true, true, false, false};
-    vector<vector<int>> map{{-1, 0, 0, 0,  0,  0,  -1},
-                            {0,  0, 0, 0,  0,  0,  -1},
-                            {-1, 0, 0, 0,  0,  0,  0},
-                            {0,  0, 0, 0,  0,  0,  0},
-                            {0,  0, 0, 0,  0,  0,  0},
-                            {0,  0, 0, 0,  0,  0,  0},
-                            {0,  0, 0, -1, -1, -1, -1}};
-
-    int possibleRun(decltype(map) &_map_, int x, int y) {
-        if (x < 0 || x > 6 || y < 0 || y > 6 || _map_[x][y])
-            return 0;
-        int _ans_ = 1;
-        _map_[x][y] = 1;
-        _ans_ += possibleRun(_map_, x - 1, y);
-        _ans_ += possibleRun(_map_, x + 1, y);
-        _ans_ += possibleRun(_map_, x, y + 1);
-        _ans_ += possibleRun(_map_, x, y - 1);
-        return _ans_;
-    }
-
-    bool possible() {
-        auto _map_ = map;
-        for (int x = 0; x < 7; ++x)
-            for (int y = 0; y < 7; ++y) {
-                if (_map_[x][y])
-                    continue;
-                int tmp = possibleRun(_map_, x, y);
-                if (tmp < 5)
-                    return false;
-            }
-        return true;
-    }
-
-    static pii cvt(const pii &start, const int dir, const pii &vec, bool reverse = false) {
-        if (reverse)
-            switch (dir) {
-                case 0:
-                    return {start.first - vec.first, start.second + vec.second};
-                case 1:
-                    return {start.first - vec.second, start.second - vec.first};
-                case 2:
-                    return {start.first + vec.first, start.second - vec.second};
-                case 3:
-                    return {start.first + vec.second, start.second + vec.first};
-                default:
-                    return {-1, -1};
-            }
-        else
-            switch (dir) {
-                case 0:
-                    return {start.first + vec.first, start.second + vec.second};
-                case 1:
-                    return {start.first + vec.second, start.second - vec.first};
-                case 2:
-                    return {start.first - vec.first, start.second - vec.second};
-                case 3:
-                    return {start.first - vec.second, start.second + vec.first};
-                default:
-                    return {-1, -1};
-            }
-    }
-
-    vector<pii> reg;
-
-    bool calculate(vector<pii> &now, int x, int y, int dir, bool rev) {
-        reg.clear();
-        for (const auto &regi: now) {
-            auto ret = cvt({x, y}, dir, regi, rev);
-            if (ret.first < 0 || ret.first > 6 || ret.second < 0 || ret.second > 6 ||
-                map[ret.first][ret.second])
-                return false;
-            reg.emplace_back(ret);
-        }
-        return true;
-    }
-
-public:
-    explicit christmas(basic_ostream<char> &out) : out(out) {}
-
-    void printMap(decltype(map) &_map) {
-        for (auto &i: _map) {
-            for (auto &j: i)
-                if (j == -1)
-                    out << '0' << ' ';
-                else
-                    out << j << ' ';
-            out << '\n';
-        }
-        out << flush;
-    }
-
-    vector<decltype(map)> ans;
-
-    bool show = false;
-
-    void run(int idx = 0) {
-        if (!possible()) {
-//            out << "impossible:\n";
-//            printMap(map);
-            return;
-        }
-        auto &now = blocks[idx].points;
-        for (int i = 0; i < 7; ++i) {
-            for (int j = 0; j < 7; ++j) {
-                if (map[i][j])
-                    continue;
-                bool rev = false;
-                again:
-                for (int dir = 0; dir < (turnable[idx] ? 4 : 2); ++dir) {
-                    if (!calculate(now, i, j, dir, rev))
-                        continue;
-                    auto tmpreg = reg;
-                    for (auto &regi: tmpreg)
-                        map[regi.first][regi.second] = idx + 1;
-//                    decltype(map) map3(7, vector<int>(7));
-//                    for (auto &regi: tmpreg)
-//                        map3[regi.first][regi.second] = idx + 1;
-//                    printMap(map3);
-                    if (idx == 7 && (ans.empty() || ans.back() != map)) {
-                        ans.push_back(map);
-                    } else {
-                        run(idx + 1);
-                    }
-                    for (auto &regi: tmpreg)
-                        map[regi.first][regi.second] = 0;
-                }
-                if (rev != reversible[idx]) {
-                    rev = true;
-                    goto again;
-                }
-            }
-        }
-    }
-};
+ofstream file;
 
 int main() {
     ios::sync_with_stdio(false);
-    ofstream file("./out.txt");
-    christmas a(file);
+    file.open("./out.txt");
+    christmas a;
+    a.setMap({"X.....X",
+              "......X",
+              "X......",
+              ".......",
+              ".......",
+              ".......",
+              "...XXXX"});
+    a.add_block({"+++", "+", "+"}, true, false);
+    a.add_block({"++++", "+"}, false, false);
+    a.add_block({"++++", " +"}, false, false);
+    a.add_block({"++", " +++"}, false, false);
+    a.add_block({"++", "+++"}, false, false);
+    a.add_block({"+ +", "+++"}, true, false);
+    a.add_block({" ++", " +", "++"}, false, true);
+    a.add_block({"++", "++", "++"}, true, true);
+    a.init();
     a.run();
-    for (auto i: a.ans) {
-        file << "\n";
-        a.printMap(i);
-    }
+    for (auto &i: a.getAns())
+        file << i << '\n';
 }
